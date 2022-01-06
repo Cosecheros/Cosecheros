882b5c12afadf29e322d4de98d0ebe14d84b92b5
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2022-01-05 21:54:57 -0300

    Fixes, ahora compila almenos

--- lib/shared/helpers.dart
+++ lib/shared/helpers.dart
@@ -20,12 +20,39 @@ Future<LatLng> getCurrentPosition() async {
 // LatLng latLngFromPosition(Position pos) => LatLng(pos.latitude, pos.longitude);
 
 Future<LatLng> getLastPosition() async {
-  var pos = await Geolocator.getLastKnownPosition();
-
-  if (pos != null) {
-    return latLngFromPosition(pos);
+  // Test if location services are enabled.
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return null;
   }
-  return null;
+
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return null;
+    }
+  }
+
+  // Permissions are denied forever, handle appropriately.
+  if (permission == LocationPermission.deniedForever) {
+    return null;
+  }
+
+  var pos = await Geolocator.getLastKnownPosition();
+  if (pos == null) {
+    return null;
+  }
+
+  return latLngFromPosition(pos);
 }
 
 LatLng latLngFromGeoPos(GeoPos pos) => LatLng(pos.latitude, pos.longitude);
