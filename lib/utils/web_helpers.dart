01593165305feb55bdf0bf73cd6d2fac3795647a
Silva Alejandro Ismael <silva.alejandro.ismael@gmail.com>
2023-01-15 17:39:08 -0300

    Fix web download, tal vez 4

--- lib/utils/web_helpers.dart
+++ lib/utils/web_helpers.dart
@@ -3,7 +3,7 @@ import 'dart:html';
 
 Future<String> download(String body, String name) async {
   print("downloading in web $name");
-  final content = base64Encode(body.codeUnits);
+  final content = base64Encode(utf8.encode(body));
   final _ = AnchorElement(
       href: "data:application/octet-stream;charset=utf-16le;base64,$content")
     ..setAttribute("download", name)
